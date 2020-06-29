3b41bb1f49a68e11df24b06e24bf6fd6007f7207
Carl Smestad <carl.smestad@gmail.com>
2020-06-29 13:58:13 +0200

    fix import

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Header from "../components/header";
-import { Reddit } from "../components/Reddit"
 import { Link } from "gatsby"
 
 const IndexPage: React.FC = () => (
