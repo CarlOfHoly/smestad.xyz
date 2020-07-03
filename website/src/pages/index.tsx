fb6fff7831f3d01c074db662140a2fe7f4a7c442
Carl Smestad <carl.smestad@gmail.com>
2020-07-03 08:01:46 +0200

    add/modify pages

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import "./index.css"
+import Header from "../components/Header/Header"
 
 const IndexPage: React.FC = () => (
-    <div>
-        <h1 className="title">Hello!</h1>
-    </div>
-) 
+  <div>
+    <Header />
+  </div>
+)
 
 export default IndexPage
