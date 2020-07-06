cba837381fe6a8f8d9dcd5626e9b1eb053b6f897
Carl Smestad <carl.smestad@gmail.com>
2020-07-06 08:35:35 +0200

    fix import

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./index.css"
 import Header from "../components/Header/Header"
 import FrontPage from "../components/FrontPage/FrontPage"
-import { Overlay } from "components/UI/Overlay/Overlay"
+import { Overlay } from "../components/UI/Overlay/Overlay"
 
 const IndexPage: React.FC = () => (
   <div className="index">
