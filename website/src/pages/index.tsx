f91e672a0ad0cb75096bcba89e9d19761f01eb33
carlofholy <carl.smestad@gmail.com>
2020-07-06 21:44:47 +0200

    fix background and overlay issues

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import "./index.css"
 import Header from "../components/Header/Header"
 import FrontPage from "../components/FrontPage/FrontPage"
-import { Overlay } from "../components/UI/Overlay/Overlay"
 
 const IndexPage: React.FC = () => (
   <div className="index">
-    <Overlay />
-    <Header />
-    <FrontPage />
+    <div className="overlay">
+      <Header />
+      <FrontPage />
+    </div>
   </div>
 )
 
