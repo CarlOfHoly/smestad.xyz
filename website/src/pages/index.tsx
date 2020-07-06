1994c09e30887149ce517e0e0f324f16ec46f2c7
Carl Smestad <carl.smestad@gmail.com>
2020-07-06 08:34:58 +0200

    fix styling

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import "./index.css"
 import Header from "../components/Header/Header"
 import FrontPage from "../components/FrontPage/FrontPage"
+import { Overlay } from "components/UI/Overlay/Overlay"
 
 const IndexPage: React.FC = () => (
   <div className="index">
+    <Overlay />
     <Header />
     <FrontPage />
   </div>
