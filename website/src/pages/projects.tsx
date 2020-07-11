cf5172ae4f5912360e33da4d819ee78fcbe631e1
carlofholy <carl.smestad@gmail.com>
2020-07-11 10:48:31 +0200

    import work

--- website/src/pages/projects.tsx
+++ website/src/pages/projects.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "../components/Header/Header"
+import Work from "../components/Work/Work"
 
 const projects: React.FC = () => {
   return (
     <div>
       <Header />
+      <Work />
     </div>
   )
 }
