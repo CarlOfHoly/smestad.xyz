46a70f2c0d1b4be64d20312f7439e7c8c4561500
carlofholy <carl.smestad@gmail.com>
2020-07-18 14:43:40 +0200

    add loading spinner google style

--- website/src/components/PracticeComponents/GSAP/Loader.tsx
+++ website/src/components/PracticeComponents/GSAP/Loader.tsx
@@ -1,12 +1,33 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import { TweenMax } from "gsap"
 
 const Loader: React.FC = () => {
+  const blue = useRef(null)
+  const red = useRef(null)
+  const yellow = useRef(null)
+  const green = useRef(null)
+
+  useEffect(() => {
+    TweenMax.fromTo(
+      [blue.current, yellow.current],
+      0.5,
+      { y: 18 },
+      { y: -18, yoyo: true, repeat: -1 }
+    )
+    TweenMax.fromTo(
+      [green.current, red.current],
+      0.5,
+      { y: -18 },
+      { y: 18, yoyo: true, repeat: -1 }
+    )
+  }, [])
+
   return (
     <svg viewBox="0 0 150 33.2" width="180" height="150">
-      <circle cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
-      <circle cx="55.2" cy="16.6" r="16.1" fill="#de4431" />
-      <circle cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
-      <circle cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
+      <circle ref={blue} cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
+      <circle ref={red} cx="55.2" cy="16.6" r="16.1" fill="#de4431" />
+      <circle ref={yellow} cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
+      <circle ref={green} cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
     </svg>
   )
 }
