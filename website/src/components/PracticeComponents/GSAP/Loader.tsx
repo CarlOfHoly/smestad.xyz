3d2944b77043090e64fad1f3d6115bce3cf9d70a
carlofholy <carl.smestad@gmail.com>
2020-07-19 22:48:01 +0200

    finish gsap getting started

--- website/src/components/PracticeComponents/GSAP/Loader.tsx
+++ website/src/components/PracticeComponents/GSAP/Loader.tsx
@@ -2,33 +2,57 @@ import React, { useRef, useEffect, useState } from "react"
 import { gsap } from "gsap"
 
 const Loader: React.FC = () => {
+  const box = useRef(null)
   const blue = useRef(null)
   const red = useRef(null)
   const yellow = useRef(null)
   const green = useRef(null)
 
-  const [messages, setMessages] = useState([])
-
-  const showMessage = message => setMessages([message])
-
   useEffect(() => {
-    gsap.to(blue.current, {
-      duration: 3,
-      x: 100,
-      onStart: showMessage,
-      onStartParams: ["Starting to move"],
-      onComplete: showMessage,
-      onCompleteParams: ["finishing"],
-      delay: 1,
+    gsap.from(".circle", {
+      duration: 1,
+      opacity: 0,
+      y: "random(-100, 50)",
+      stagger: 0.25,
     })
   }, [])
 
   return (
     <>
-      <svg viewBox="0 0 150 33.2" width="180" height="150">
-        <circle ref={blue} cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
+      <svg ref={box} width="180" height="150">
+        <circle
+          className="circle"
+          ref={blue}
+          cx="16.1"
+          cy="16.6"
+          r="16.1"
+          fill="#527abd"
+        />
+        <circle
+          className="circle"
+          ref={red}
+          cx="55.2"
+          cy="16.6"
+          r="16.1"
+          fill="#de4431"
+        />
+        <circle
+          className="circle"
+          ref={yellow}
+          cx="94.3"
+          cy="16.6"
+          r="16.1"
+          fill="#f4b61a"
+        />
+        <circle
+          className="circle"
+          ref={green}
+          cx="133.4"
+          cy="16.6"
+          r="16.1"
+          fill="#009e52"
+        />
       </svg>
-      {messages}
     </>
   )
 }
