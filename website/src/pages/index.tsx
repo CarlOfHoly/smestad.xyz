bd121aed4dc6d8c9c740347e70c51fc1a3ea7a10
Carl Smestad <carl.smestad@gmail.com>
2020-07-21 09:42:49 +0200

    add default fullpage component

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,16 +1,37 @@
 import React from "react"
-import "./index.css"
-import FrontPage from "../components/FrontPage/FrontPage"
 import Layout from "../components/layout"
+import ReactFullpage from "@fullpage/react-fullpage"
+import "./index.css"
 
-const IndexPage: React.FC = () => (
-  <div className="index">
-    <div className="overlay">
-      <Layout>
-        <FrontPage />
-      </Layout>
-    </div>
-  </div>
-)
+const IndexPage: React.FC = () => {
+  return (
+    <Layout>
+      <ReactFullpage
+        scrollingSpeed={1000}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <p>Section 1 (welcome to fullpage.js)</p>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Click me to move down
+                </button>
+              </div>
+              <div className="section">
+                <p>Section 2</p>
+              </div>
+              <div className="section">
+                <p>Section 3</p>
+              </div>
+              <div className="section">
+                <p>Section 4</p>
+              </div>
+            </ReactFullpage.Wrapper>
+          )
+        }}
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
