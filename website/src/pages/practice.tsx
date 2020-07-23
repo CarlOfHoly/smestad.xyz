87d79550813a0af6d999087f77af891ded1d2572
Carl Smestad <carl.smestad@gmail.com>
2020-07-23 08:55:20 +0200

    wrap portal with profiler for stats

--- website/src/pages/practice.tsx
+++ website/src/pages/practice.tsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { Profiler } from "react"
 import Layout from "../components/layout"
 import Animations from "../components/PracticeComponents/GSAP/Animations"
 import Portal from "../components/PracticeComponents/Portal"
 
 const PracticePage: React.FC = () => {
+  const callback = (id, phase, actualTime, baseTime, startTime, commitTime) => {
+    console.log("id: " + id)
+    console.log("phase: " + phase)
+    console.log("actualTime: " + actualTime)
+    console.log("baseTime: " + baseTime)
+    console.log("startTime: " + startTime)
+    console.log("commitTime: " + commitTime)
+  }
   return (
     <Layout>
-      <Portal />
-      <Animations />
+      <Profiler id="Navigation" onRender={callback}>
+        <Portal />
+      </Profiler>
+      {/* <Animations /> */}
     </Layout>
   )
 }
