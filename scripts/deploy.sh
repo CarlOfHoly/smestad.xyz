14b37ec614f394c79f13c6aab640827130694614
carlofholy <carl.smestad@gmail.com>
2020-07-23 22:56:34 +0200

    update secret

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,13 @@ set -e
 # Pipe our environmental SSH key variable into a file
 mkdir -p $HOME/.ssh
 echo "${deploy_key}" > $HOME/.ssh/deploy_key
-echo "${ls $HOME/.ssh/}"
 chmod 600 $HOME/.ssh/deploy_key # SSH keys need to be readonly
 
 # Where to deploy our site on our server
-target="/var/www/cabin/"
+target="~/html"
 
 # The actual rsync
-sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/* --rsync-path='mkdir -p ${target} && rsync' root@smestad.xyz:${target}"
+sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/* --rsync-path='mkdir -p ${target} && rsync' root@:smestad.xyz${target}"
 
 # Remove our deploy_key again since it's no longer needed
 rm $HOME/.ssh/deploy_key
