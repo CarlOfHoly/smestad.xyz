13bbe6c4c0a5a5053a8c7f796433bb06f3ec5d69
carlofholy <carl.smestad@gmail.com>
2020-07-23 23:37:52 +0200

    try stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
-
-# Instantly exists our script whenever an error occurs
 set -e
 
-# Pipe our environmental SSH key variable into a file
 mkdir -p $HOME/.ssh
 echo "${deploy_key}" > $HOME/.ssh/deploy_key
 chmod 600 $HOME/.ssh/deploy_key # SSH keys need to be readonly
 
-# Where to deploy our site on our server
-target="/var/www/cabin"
+target="/var/www/cabin/"
 
-# The actual rsync
 sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/*  && rsync' root@smestad.xyz:${target}"
 
-# Remove our deploy_key again since it's no longer needed
 rm $HOME/.ssh/deploy_key
