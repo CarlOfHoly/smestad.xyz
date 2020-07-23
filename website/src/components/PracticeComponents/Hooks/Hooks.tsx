50f366448a157f9e2411603ab86e0a035bdd3c2a
Carl Smestad <carl.smestad@gmail.com>
2020-07-23 10:00:00 -0600

    test setStatehooks

--- website/src/components/PracticeComponents/Hooks/Hooks.tsx
+++ website/src/components/PracticeComponents/Hooks/Hooks.tsx
@@ -1,5 +1,20 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import "./Hooks.css"
 
-const Hooks: React.FC = () => <div>"This is a test"</div>
+const Hooks: React.FC = () => {
+  const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 })
 
+  return (
+    <div className="hooks-practice">
+      <button
+        className="button"
+        onClick={() => setCount(currentCount => currentCount + 1)}
+      >
+        Inc counter
+      </button>
+      <div className="count">{count1}</div>
+      <div className="count">{count2}</div>
+    </div>
+  )
+}
 export default Hooks
