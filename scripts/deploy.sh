93c2c6b74c0d912d21c22ea9d843f76d6eb53609
carlofholy <carl.smestad@gmail.com>
2020-07-23 23:41:31 +0200

    revert changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ chmod 600 $HOME/.ssh/deploy_key # SSH keys need to be readonly
 
 target="/var/www/cabin/"
 
-sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/*  && rsync' root@smestad.xyz:${target}"
+sh -c "rsync -azh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' public/* --rsync-path='mkdir -p ${target} && rsync' root@smestad.xyz:${target}"
 
 rm $HOME/.ssh/deploy_key
