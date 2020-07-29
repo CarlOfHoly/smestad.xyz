b71ce82a02f6eff9f744ef188d71282c7ee82e06
carlofholy <carl.smestad@gmail.com>
2020-07-29 19:36:13 +0200

    add theme-toggler

--- website/src/components/Header/Header.tsx
+++ website/src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react"
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu"
 import Nav from "../Nav/Nav"
+import ThemeToggler from "./ThemeToggler"
 
-const Header: React.FC = () => {
+interface Props {
+  on: boolean
+  toggle: () => void
+}
+
+const Header: React.FC<Props> = ({ on, toggle }) => {
   const [showMenu, setShowMenu] = useState<boolean>(false)
 
   const clicked = () => {
@@ -11,6 +17,7 @@ const Header: React.FC = () => {
 
   return (
     <header>
+      <ThemeToggler on={on} toggle={toggle} />
       <BurgerMenu show={!showMenu} click={clicked} />
       <Nav show={showMenu} />
     </header>
