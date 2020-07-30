929994a498a98bef293fd5c7b1d7fe52ef0b6fa5
carlofholy <carl.smestad@gmail.com>
2020-07-30 22:52:57 +0200

    load main component

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout/layout"
 import FullPage from "../components/FullPage/FullPage"
 import Page from "../components/FullPage/Page"
@@ -7,16 +7,24 @@ import ThemeContextProvider from "../components/Theme/ThemeContext"
 import "../../css/pages/global.css"
 
 const IndexPage: React.FC = () => {
-  return (
-    <ThemeContextProvider>
-      <Layout>
+  const [mainComponent, setMainComponent] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMainComponent(
         <FullPage scrollingSpeed={1500}>
           <Page title="Carl Smestad" titlePosition="left" />
           <Page title="About Me" titlePosition="right" />
           <Page title="My Projects" titlePosition="left" />
           <Page title="Contact Me" titlePosition="left" />
         </FullPage>
-      </Layout>
+      )
+    })
+  })
+
+  return (
+    <ThemeContextProvider>
+      <Layout>{mainComponent || <p>Loading page ...</p>}</Layout>
     </ThemeContextProvider>
   )
 }
