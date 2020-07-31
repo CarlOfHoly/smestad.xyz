79b80b0dd4a30453e6462482e42279dde485e280
Carl Smestad <carl.smestad@gmail.com>
2020-07-31 17:33:31 +0200

    add console logs for debugging

--- website/src/components/Theme/ThemeContext.tsx
+++ website/src/components/Theme/ThemeContext.tsx
@@ -15,8 +15,16 @@ const windowGlobal = typeof window !== "undefined" && window
 const ThemeContextProvider: React.FC<Props> = ({ children }) => {
   const fetchThemeFromLocalStorage = () => {
     try {
+      console.log("Trying to fetch from local storage: ")
+      console.log("Current theme is: " + theme)
+
       return JSON.parse(windowGlobal.localStorage.getItem("theme"))
     } catch (e) {
+      console.log("Failed to fetch theme from local storage")
+      console.log(
+        "using default theme which is " + ThemeContextDefaultValue.theme
+      )
+
       return ThemeContextDefaultValue.theme
     }
   }
