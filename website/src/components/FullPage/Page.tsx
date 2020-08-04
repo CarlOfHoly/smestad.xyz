24bcba902339678cad7fcaf72a7bcf11d70a0839
carlofholy <carl.smestad@gmail.com>
2020-08-04 08:09:51 +0200

    add byline and ingress with some styling

--- website/src/components/FullPage/Page.tsx
+++ website/src/components/FullPage/Page.tsx
@@ -4,15 +4,25 @@ import "../../../css/components/FullPage/Page.css"
 interface Props {
   title: String
   titlePosition: String
+  byline?: String
+  ingress?: String
 }
-const Page: React.FC<Props> = ({ title, titlePosition }) => {
+const Page: React.FC<Props> = ({ title, titlePosition, byline, ingress }) => {
   const newTitle = title.split(" ")
+  const splitIngress = ingress.split("/n")
+
   return (
     <div className={"page " + titlePosition}>
       {newTitle.map((title, index) => (
         <h1 key={index} className="title">
           {title.toUpperCase()}
         </h1>
+      ))}
+      <p className="byline">{byline}</p>
+      {splitIngress.map((ingressPart, index) => (
+        <p key={index} className="ingress">
+          {ingressPart}
+        </p>
       ))}
     </div>
   )
