5b077415ba5df056dc2483258039d68f245a6867
carlofholy <carl.smestad@gmail.com>
2020-08-05 20:02:42 +0200

    create debug component

---
+++ website/src/components/Utils/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react"
+
+const ThemeContextDefaultValue = {
+  theme: "dark-mode",
+  setTheme: (_theme: any) => {},
+}
+export const ThemeContext = React.createContext(ThemeContextDefaultValue)
+
+interface Props {
+  children?: React.ReactNode
+}
+
+const windowGlobal = typeof window !== "undefined" && window
+
+const ThemeContextProvider: React.FC<Props> = ({ children }) => {
+  const fetchThemeFromLocalStorage = () => {
+    try {
+      return JSON.parse(windowGlobal.localStorage.getItem("theme"))
+    } catch (e) {
+      return ThemeContextDefaultValue.theme
+    }
+  }
+  const [theme, setTheme] = useState(fetchThemeFromLocalStorage())
+
+  useEffect(() => {
+    windowGlobal.localStorage.setItem("theme", JSON.stringify(theme))
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeContextProvider
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,54 +1,57 @@
-import React, { useState } from "react"
+import React from "react"
 import Layout from "../components/Layout/layout"
 import FullPage from "../components/FullPage/FullPage"
 import Page from "../components/FullPage/Page"
 import Header from "../components/Header/Header"
-import ThemeContextProvider from "../components/Theme/ThemeContext"
+import ThemeContextProvider from "../components/Utils/ThemeContext"
 import Spinner from "../components/UI/Spinner"
 import useSpinner from "../components/CustomHooks/useSpinner"
 import Circle from "../components/UI/Canvas/Circle"
+import Debug from "../components/Utils/Debug"
 
 import "../../css/pages/index.css"
 
 const IndexPage: React.FC = () => {
   return (
     <ThemeContextProvider>
-      <Circle />
       <Layout>
-        {useSpinner(
-          <div className="spinner">
-            <Spinner />
-          </div>,
-          <>
-            <Header />
-            <FullPage scrollingSpeed={1500}>
-              <Page
-                title="Carl Smestad"
-                titlePosition="left"
-                byline="square"
-                ingress={"full-stack developer / /n procrastinator"}
-              />
-              <Page
-                title="About Me"
-                titlePosition="left"
-                byline="triangle"
-                ingress="click the triangles to check /n out my resume"
-              />
-              <Page
-                title="My Projects"
-                titlePosition="left"
-                byline="circle"
-                ingress="check out some of /n my projects"
-              />
-              <Page
-                title="Contact Me"
-                titlePosition="left"
-                ingress="full-stack developer / procrastinator"
-              />
-            </FullPage>
-          </>,
-          1000
-        )}
+        <Circle />
+        <Debug>
+          {useSpinner(
+            <div className="spinner">
+              <Spinner />
+            </div>,
+            <>
+              <Header />
+              <FullPage scrollingSpeed={1500}>
+                <Page
+                  title="Carl Smestad"
+                  titlePosition="left"
+                  byline="square"
+                  ingress={"full-stack developer / /n procrastinator"}
+                />
+                <Page
+                  title="About Me"
+                  titlePosition="left"
+                  byline="triangle"
+                  ingress="click the triangles to check /n out my resume"
+                />
+                <Page
+                  title="My Projects"
+                  titlePosition="left"
+                  byline="circle"
+                  ingress="check out some of /n my projects"
+                />
+                <Page
+                  title="Contact Me"
+                  titlePosition="left"
+                  ingress="full-stack developer / procrastinator"
+                />
+              </FullPage>
+            </>,
+            1000
+          )}
+        </Debug>
       </Layout>
     </ThemeContextProvider>
   )
