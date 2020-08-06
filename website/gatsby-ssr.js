e2e0e0f4c5f586adef3a0926dcc4b758fd8f4a6d
Carl Smestad <carl.smestad@gmail.com>
2020-08-06 08:13:51 +0200

    change gatsby-ssr to commonJS from ES6

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -1,4 +1,5 @@
-import React from "react"
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const React = require("react")
 
 exports.onRenderBody = ({ setPreBodyComponents }) => {
   setPreBodyComponents([
