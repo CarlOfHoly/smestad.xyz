e64cd082e8b352bc034b90d7be3971d66dee01ce
carlofholy <carl.smestad@gmail.com>
2020-08-10 15:01:02 +0200

    add background-blobs to index

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,61 +6,50 @@ import Header from "../components/Header/Header"
 import ThemeContextProvider from "../components/Utils/ThemeContext"
 import Spinner from "../components/UI/Spinner"
 import useSpinner from "../components/CustomHooks/useSpinner"
-import Circle from "../components/UI/Canvas/Circle"
-import Canvas from "../components/UI/Canvas/Canvas"
+import Blobs from "../components/UI/Canvas/Blobs"
 import Debug from "../components/Utils/Debug"
 
 import "../../css/pages/index.css"
 
 const IndexPage: React.FC = () => {
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = "#000000"
-    ctx.beginPath()
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
-    ctx.fill()
-  }
   return (
     <ThemeContextProvider>
       <Layout>
-        <Canvas draw={draw} />
-        <Circle />
-        <Debug>
-          {useSpinner(
-            <div className="spinner">
-              <Spinner />
-            </div>,
-            <>
-              <Header />
-              <FullPage scrollingSpeed={1500}>
-                <Page
-                  title="Carl Smestad"
-                  titlePosition="left"
-                  byline="square"
-                  ingress={"full-stack developer / /n procrastinator"}
-                />
-                <Page
-                  title="About Me"
-                  titlePosition="left"
-                  byline="triangle"
-                  ingress="click the triangles to check /n out my resume"
-                />
-                <Page
-                  title="My Projects"
-                  titlePosition="left"
-                  byline="circle"
-                  ingress="check out some of /n my projects"
-                />
-                <Page
-                  title="Contact Me"
-                  titlePosition="left"
-                  ingress="full-stack developer / procrastinator"
-                />
-              </FullPage>
-            </>,
-            1000
-          )}
-        </Debug>
+        <Blobs />
+        {useSpinner(
+          <div className="spinner">
+            <Spinner />
+          </div>,
+          <>
+            <Header />
+            <FullPage scrollingSpeed={1500}>
+              <Page
+                title="Carl Smestad"
+                titlePosition="left"
+                byline="square"
+                ingress={"full-stack developer / /n procrastinator"}
+              />
+              <Page
+                title="About Me"
+                titlePosition="left"
+                byline="triangle"
+                ingress="click the triangles to check /n out my resume"
+              />
+              <Page
+                title="My Projects"
+                titlePosition="left"
+                byline="circle"
+                ingress="check out some of /n my projects"
+              />
+              <Page
+                title="Contact Me"
+                titlePosition="left"
+                ingress="full-stack developer / procrastinator"
+              />
+            </FullPage>
+          </>,
+          1000
+        )}
       </Layout>
     </ThemeContextProvider>
   )
