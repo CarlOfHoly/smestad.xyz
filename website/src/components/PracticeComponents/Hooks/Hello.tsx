339f546f143f1d6c943bc18ada19f97b7bc4df9d
carlofholy <carl.smestad@gmail.com>
2020-08-11 19:02:57 +0200

    cleanup

--- website/src/components/PracticeComponents/Hooks/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useRef } from "react"
-
-export const Hello: React.FC = () => {
-  const renders = useRef(0)
-
-  console.log("hello renders: ", renders.current++)
-  return <h1>Hello</h1>
-}
