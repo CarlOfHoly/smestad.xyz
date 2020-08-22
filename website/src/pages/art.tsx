62267d4ac0dfa3688bdd3fcc1d5b8f1b8757bf5b
carlofholy <carl.smestad@gmail.com>
2020-08-22 15:58:05 +0200

    display Tron

--- website/src/pages/art.tsx
+++ website/src/pages/art.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import GalacticTrails from "../components/UI/Canvas/GalacticTrails"
+import Tron from "../components/UI/Canvas/Tron"
 
 const Art = () => {
-  return <GalacticTrails />
+  return <Tron />
 }
 
 export default Art
