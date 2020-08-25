dcf1bf713be345d1e1b52aa15e7df89519431906
carlofholy <carl.smestad@gmail.com>
2020-08-25 08:33:33 +0200

    add test component

--- website/src/pages/projects.tsx
+++ website/src/pages/projects.tsx
@@ -1,14 +1,16 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout/layout"
 import ThemeContextProvider from "../components/Utils/ThemeContext"
 import Header from "../components/Header/Header"
+import Person from "../components/Person"
 
 const Projects: React.FC = () => {
   return (
     <ThemeContextProvider>
       <Layout>
         <Header />
-        PROJECTS
+        <Person />
       </Layout>
     </ThemeContextProvider>
   )
