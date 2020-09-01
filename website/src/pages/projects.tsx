7af67b6dec266e310728bdbd222221c1e8c21ace
carlofholy <carl.smestad@gmail.com>
2020-09-01 12:19:07 +0200

    add project component

--- website/src/pages/projects.tsx
+++ website/src/pages/projects.tsx
@@ -2,13 +2,23 @@ import React from "react"
 import Layout from "../components/Layout/layout"
 import ThemeContextProvider from "../components/Utils/ThemeContext"
 import Header from "../components/Header/Header"
+import Project from "../components/Project/Project"
 
 const Projects: React.FC = () => {
   return (
     <ThemeContextProvider>
       <Layout>
         <Header />
-        PROJECTS
+        <Project
+          projectTitle="Project 1"
+          projectIngress="Ingress"
+          reverse={false}
+        />
+        <Project
+          projectTitle="Project 2"
+          projectIngress="Ingress"
+          reverse={true}
+        />
       </Layout>
     </ThemeContextProvider>
   )
